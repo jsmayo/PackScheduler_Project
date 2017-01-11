e0a46ad7518dd413fe1a90e60da38640119226f4
jsmayo <jsmayo@ncsu.edu>
2017-01-11 18:42:37 -0500

    Corrected Student code

--- PackScheduler/src/edu/ncsu/csc216/pack_scheduler/user/Student.java
+++ PackScheduler/src/edu/ncsu/csc216/pack_scheduler/user/Student.java
@@ -1,171 +1,177 @@
 package edu.ncsu.csc216.pack_scheduler.user;
 
+/**
+ * Creates a Student Object to be used with the StudentRecordIO class.
+ * Students are only created if the input is valid. Conditions exist for
+ * the Student's: first name, last name, id, email, password, and maximum credit hours.
+ * @author Steven Mayo with partial skeleton code from Sarah Heckman
+ */
 public class Student {
-
+	/** String for student's first name */
 	private String firstName;
+	/** String for student's last name */
 	private String lastName;
-	private String id;
+	/** String for student's email address */
 	private String email;
-	private String password;
+	/** String for student's identification tag */
+	private String id;
+	/** String for student's pre-hashed password */
+	private String hashedPassword;
+	/** Integer for the maximum number of credits available to the student */
 	private int maxCredits;
-	static final int MAX_CREDITS = 18;
-	
+	/** The default maximum number of credits allowed at NC State */
+	public static final int MAX_CREDITS = 18;
+
 	/**
-	 * Constructor for Student object. 
-	 * @param firstName Students first name. 
-	 * @param lastName Students last name.
-	 * @param id Students ID number.
-	 * @param email Students email address. 
-	 * @param hashPW Students password.
-	 * @param maxCredits Students maximum credit allocation. 
+	 * Student constructor that creates a new student object from passed in parameters using setter methods.
+	 * @param firstName Student's first name.
+	 * @param lastName Student's last name.
+	 * @param id Students identification tag.
+	 * @param email Student's email address.
+	 * @param hashedPassword A pre-hashed password for the students account.
+	 * @param maxCredits The maximum number of credits available to the student. 
 	 */
-	public Student(String firstName, String lastName, String id, String email, String hashPW, int maxCredits) {
+	public Student(String firstName, String lastName, String id, String email, String hashedPassword, int maxCredits)  {
 		setFirstName(firstName);
 		setLastName(lastName);
 		setId(id);
 		setEmail(email);
-		setPassword(hashPW);
+		setPassword(hashedPassword);
 		setMaxCredits(maxCredits);
+		
 	}
 	
 	/**
-	 * Constructor for a Student Object that lacks a maxCredit parameter. 
-	 * @param firstName Students first name.
-	 * @param lastName Students last name.
-	 * @param id Students ID.
-	 * @param email Students email.
-	 * @param hashPW Students password.
+	 * An overloaded constructor for the Student Object. It uses the maximum number of courses a student can take
+	 * at NC State if one is not provided initially. 
+	 * @param firstName Student's first name.
+	 * @param lastName Student's last name.
+	 * @param id Student's identification tag.
+	 * @param email Student's email address.
+	 * @param hashedPassword Student's pre-hashed password. 
 	 */
-	public Student(String firstName, String lastName, String id, String email, String hashPW) {
-		this(firstName, lastName, id, email, hashPW, 18);
+	public Student(String firstName, String lastName, String id, String email, String hashedPassword) {
+		this(firstName, lastName, id, email, hashedPassword, MAX_CREDITS); //passes to other constructor with a default value of 18 for credits.
 	}
-
-
+	
 	/**
-	 * Returns the Students first name.
-	 * @return the firstName
+	 * Retrieves the student's first name.
+	 * @return firstName
 	 */
 	public String getFirstName() {
 		return firstName;
 	}
-
-
+	
 	/**
-	 * Sets the Students first name.
-	 * @param firstName the firstName to set
-	 */
-	public void setFirstName(String firstName) {
-		if(firstName == null || firstName.isEmpty()) throw new IllegalArgumentException();
-		this.firstName = firstName;
-	}
-
-
-	/**
-	 * Returns the Students last name.
-	 * @return the lastName
+	 * Retrieves the student's last name.
+	 * @return lastName
 	 */
 	public String getLastName() {
+	
 		return lastName;
 	}
-
-
+	
 	/**
-	 * Sets the Students last name.
-	 * @param lastName the lastName to set
-	 */
-	public void setLastName(String lastName) {
-		if(lastName == null || lastName.isEmpty()) throw new IllegalArgumentException();
-		this.lastName = lastName;
-	}
-
-
-	/**
-	 * Returns the Students ID.
-	 * @return the id
-	 */
+	 * Retrieves the student's identification tag.
+	 * @return id
+	 */	
 	public String getId() {
 		return id;
 	}
 
-
 	/**
-	 * Sets the Students ID.
-	 * @param id the id to set
+	 * Retrieves the student's email address.
+	 * @return email
 	 */
-	private void setId(String id) {
-		if(id == null || id.isEmpty()) throw new IllegalArgumentException();
-		this.id = id;
-	}
-
-
-	/**
-	 * Returns the Students email.
-	 * @return the email
-	 */
-	public String getEmail() {
+	public String getEmail(){
 		return email;
 	}
 
-
 	/**
-	 * Sets the Students email.
-	 * @param email the email to set
+	 * Retrieves the student's password, which was passed in as a hashed value.
+	 * @return hashedPassword
 	 */
-	public void setEmail(String email) {
-		if(email == null || email.isEmpty()) throw new IllegalArgumentException();
-		int atIndex = 0;
-		int dotIndex = 0;
-		for(int i = 0; i < email.length(); i++){
-			if(email.charAt(i) == '@') atIndex = i;
-			if(email.charAt(i) == '.') dotIndex = i;
-		}
-		if(dotIndex >= atIndex || dotIndex == 0) throw new IllegalArgumentException(". before @");
-		if(atIndex <= 0) throw new IllegalArgumentException("Email must have @ symbol.");
-		this.email = email;
+	public String getPassword(){
+		return hashedPassword;
 	}
-
-
+	
 	/**
-	 * Returns the Students password.
-	 * @return the password
+	 * Retrieves the maximum number of course available to the student.
+	 * @return maxCredits
 	 */
-	public String getPassword() {
-		return password;
-	}
-
-
-	/**
-	 * Sets the Students password.
-	 * @param password the password to set
-	 */
-	public void setPassword(String password) {
-		if(password == null || password.isEmpty()) throw new IllegalArgumentException("Password cannot be null or empty.");
-		this.password = password;
-	}
-
-
-	/**
-	 * Returns the students maximum number of credits.
-	 * @return the maxCredits
-	 */
-	public int getMaxCredits() {
+	public int getMaxCredits(){
 		return maxCredits;
 	}
-
-
+	
 	/**
-	 * Sets the maximum number of credits allowed for the Student.
-	 * @param maxCredits the maxCredits to set
+	 * Sets the students first name,  which is invalid if it is a null or empty string.
+	 * @param firstName The students first name
+	 * @throws IllegalArgumentException if the first name is invalid
 	 */
-	public void setMaxCredits(int maxCredits) {
-		if(maxCredits < 3 || maxCredits > 18) throw new IllegalArgumentException("Max credits cannot be less than 3 or greater than 18.");
+	public void setFirstName(String firstName){
+		if(firstName == null || firstName.isEmpty()) throw new IllegalArgumentException("Invalid first name");
+		this.firstName = firstName;
+	}
+	
+	/**
+	 * Sets the student's last name,  which is invalid if it is a null or empty string.
+	 * @param lastName The students last name.
+	 * @throws IllegalArgumentException if the last name is invalid
+	 */
+	public void setLastName(String lastName){
+		if(lastName == null || lastName.isEmpty()) throw new IllegalArgumentException("Invalid last name");
+		this.lastName = lastName;
+	}
+	
+	/**
+	 * Set's the student's identification tag, which is invalid if it is a null or empty string.
+	 * @param id The student's identification tag.
+	 * @throws IllegalArgumentException if the ID is invalid.
+	 */
+	public void setId(String id){
+		if(id == null || id.isEmpty()) throw new IllegalArgumentException("Invalid id");
+		if(id.contains("@")) throw new IllegalArgumentException("Invalid id");
+		this.id = id;
+	}
+	
+	/**
+	 * Sets the student's email address, which is invalid if it is a null or empty string. Additionally,
+	 * the student's email address must contain the "@" and "." characters with the last index of the "." appearing
+	 * after the "@" character.
+	 * @param email The student's email address
+	 * @throws IllegalArgumentException if the email is invalid.
+	 */
+	public void setEmail(String email){
+		if(email == null || email.isEmpty()) throw new IllegalArgumentException("Invalid email");
+		if( !(email.contains(".")) || !(email.contains("@"))) throw new IllegalArgumentException("Invalid email");
+		if(email.lastIndexOf(".") < email.lastIndexOf("@")) throw new IllegalArgumentException("Invalid email");
+		this.email = email;
+	}
+	
+	/**
+	 * Sets the student's pre-hashed password, which is invalid if it is a null or empty string.
+	 * @param hashedPassword The student's password that was passed in pre-hashed via "SHA-256".
+	 * @throws IllegalArgumentException if the password is invalid.
+	 */
+	public void setPassword(String hashedPassword){
+		if(hashedPassword == null || hashedPassword.isEmpty()) throw new IllegalArgumentException("Invalid password");
+		this.hashedPassword = hashedPassword;
+	}
+	
+	/**
+	 * Sets the student's maximum number of credits available to that particular student, which must be greater than
+	 * 3 hours and less than 18 hours.
+	 * @param maxCredits The student's maximum number of credits they can register for.
+	 * @throws IllegalArgumentException if exceeding or falling short of the requirements of NC State.
+	 */
+	public void setMaxCredits(int maxCredits){
+		if(maxCredits < 3 || maxCredits > 18) throw new IllegalArgumentException("Invalid max credits");
 		this.maxCredits = maxCredits;
 	}
 
-	
-	/** 
-	 * Generates a hashCode for Student using all fields.
-	 * @return hashCode for Student.
+	/**
+	 * Generates a hashcode for the Student class using all fields.
+	 * @return hashCode for Student class.
 	 */
 	@Override
 	public int hashCode() {
@@ -176,15 +182,14 @@ public class Student {
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		result = prime * result + ((lastName == null) ? 0 : lastName.hashCode());
 		result = prime * result + maxCredits;
-		result = prime * result + ((password == null) ? 0 : password.hashCode());
+		result = prime * result + ((hashedPassword == null) ? 0 : hashedPassword.hashCode());
 		return result;
 	}
 
-
-	/** 
-	 * Compares a given Object to this object for equality on all fields.
-	 * @param obj The Object to compare.
-	 * @return true if the objects are the same on all fields. 
+	/**
+	 * Compares a given object to this object for equality on all fields.
+	 * @param obj the object to compare.
+	 * @return will return true if the objects are the same on all fields.
 	 */
 	@Override
 	public boolean equals(Object obj) {
@@ -217,23 +222,21 @@ public class Student {
 			return false;
 		if (maxCredits != other.maxCredits)
 			return false;
-		if (password == null) {
-			if (other.password != null)
+		if (hashedPassword == null) {
+			if (other.hashedPassword != null)
 				return false;
-		} else if (!password.equals(other.password))
+		} else if (!hashedPassword.equals(other.hashedPassword))
 			return false;
 		return true;
 	}
 
-
 	/**
-	 * Returns a comma separated value String of all Student fields.
-	 * @return String representation of Student.
+	 * Returns a comma separated value String of all student fields/
+	 * @return String representation of Student
 	 */
 	@Override
 	public String toString() {
-		return "firstName,lastName,id,email,password,maxCredits";
+		return firstName + "," + lastName + "," + id + "," + email + "," + hashedPassword + "," + maxCredits;
 	}
-	
 }
-	
+		
