bd9eb2bc2cfe1659b003e9b9dd1d28ff6a49a212
jsmayo <Steven@192.168.0.9>
2017-02-24 16:45:42 -0500

    Created InvalidTransitionException class.
    Specifies "Invalid FSM Transition" as the default exception message.

---
+++ PackScheduler/src/edu/ncsu/csc216/pack_scheduler/course/validator/InvalidTransitionException.java
@@ -0,0 +1,31 @@
+package edu.ncsu.csc216.pack_scheduler.course.validator;
+
+/**
+ * Custom checked exception class that throws a checked
+ * exception when the user attempts to perform an invalid
+ * state transition.
+ * 
+ *@author Steven Mayo
+ */
+public class InvalidTransitionException extends Exception {
+
+	/** Default serialVersionUID variable declared for Exception. */
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Parameterless constructor that provides a default message of "Invalid
+	 * FSM Transition." 
+	 */
+	public InvalidTransitionException() {
+		this("Invalid FSM Transition");
+	}
+	
+	/**
+	 * Constructor that accepts a string argument that is either
+	 * specified or left as the default message. 
+	 * @param s String specified as the exception message.
+	 */
+	public InvalidTransitionException(String s) {
+		super(s);
+	}
+}
