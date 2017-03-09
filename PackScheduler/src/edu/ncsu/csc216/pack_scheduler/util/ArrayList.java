d9310af4e9ba5fad557a62b05445e1b614ef6abb
jsmayo <Steven@192.168.0.4>
2017-03-08 23:12:02 -0500

    Added JavaDoc to ArrayList class and removed last checkstyle warning.

--- PackScheduler/src/edu/ncsu/csc216/pack_scheduler/util/ArrayList.java
+++ PackScheduler/src/edu/ncsu/csc216/pack_scheduler/util/ArrayList.java
@@ -1,7 +1,7 @@
 package edu.ncsu.csc216.pack_scheduler.util;
 
 import java.util.Arrays;
-import java.util.Collections;
+
 
 /**
  * Generic version of the AbstractList class.
@@ -30,6 +30,19 @@ public class ArrayList<E> extends java.util.AbstractList<E> {
 		
 	}
 	
+	/**
+	 * Adds an object of type E to this objects Array at the specified index value. 
+	 * If the size of the Array is approaching the capacity, then the Array will 
+	 * automatically double in capacity. IF a null value is passed in as an
+	 * argument or if an index value is passed in that is outside of the 
+	 * bounds, then an exception will be thrown.
+	 * @param index The index value to insert the object into the ArrayList.
+	 * @param e The object, of type E, to place at the specified index. 
+	 * @throws IndexOutOfBoundsException if the index is outside of the array
+	 * boundaries
+	 * @throws IllegalArgumnetException if the specified value is a duplicate already
+	 * found within the list.
+	 */
 	@Override
 	public void add(int index, E e){
 		if(e == null) throw new NullPointerException();
@@ -63,19 +76,31 @@ public class ArrayList<E> extends java.util.AbstractList<E> {
 		}
 		
 	}
-
+	
+	/**
+	 * Returns the value, of type E, located at the specified index value. 
+	 * @return E Object stored at the specified index value. 
+	 */
 	@Override
 	public E get(int index) {
 		return list[index];
 		//return arrayList.get(index);
 	}
-
+	
+	/**
+	 * Returns the size of the list.
+	 * @return int Size of the list. 
+	 */
 	@Override
 	public int size() {
 		return this.size;
 		//return arrayList.size();
 	}
 	
+	/**
+	 * Returns the capacity of the list. Mainly used to ensure proper functionality.
+	 * @return int Capacity of the list. 
+	 */
 	public int capacity(){
 		return this.capacity;
 	}
