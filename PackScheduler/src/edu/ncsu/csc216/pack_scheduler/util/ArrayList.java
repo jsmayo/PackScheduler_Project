a247f7120753d559fd9578881a0f9f3ffe5734b0
jsmayo <Steven@192.168.0.4>
2017-03-08 22:58:24 -0500

    Implemented add() to ArrayList and ArrayListTest classes.

--- PackScheduler/src/edu/ncsu/csc216/pack_scheduler/util/ArrayList.java
+++ PackScheduler/src/edu/ncsu/csc216/pack_scheduler/util/ArrayList.java
@@ -15,28 +15,69 @@ public class ArrayList<E> extends java.util.AbstractList<E> {
 	private E[] list;
 	/** Size of the ArrayList */
 	private int size;
-	private ArrayList<E> arrayList;
+	//private ArrayList<E> arrayList;
+	private int capacity;
 	
 	/**
 	 * Constructor for the ArrayList.
 	 */
 	@SuppressWarnings("unchecked")
 	public ArrayList() {
-		list = (E[]) new Object[10];
-		arrayList = new ArrayList<E>();
-		Collections.addAll(arrayList, list);
+		list = (E[]) new Object[INIT_SIZE];
+		this.size = 0;
+		this.capacity = 10;
+		//arrayList = new ArrayList<E>();
+		
+	}
+	
+	@Override
+	public void add(int index, E e){
+		if(e == null) throw new NullPointerException();
+		if(index > this.size || index < 0 ) throw new IndexOutOfBoundsException();
+		for(int i = 0; i < this.size; i++) if(e.equals(list[i])) throw new IllegalArgumentException("duplicate found");
+		//size is last place of value (NOT INDEXED).
+		if(this.size == 0) list[0] = e;
+		//if the size is 0, just add the value.
+		else {
+			if(index == 0) {
+				for(int i = this.size(); i > 0; i--) list[i] = list[i-1];
+				list[0] = e;
+			}
+			else {
+				for(int i = this.size; i >= index; i--) list[i] = list[i - 1];
+				list[index] =  e;
+			}
+		}
+		this.size++;
+		//if the size of the array is reaching the capacity, double the capacity.
+		if(this.size >= this.capacity) {
+			this.capacity *= 2;
+			this.list = Arrays.copyOf(this.list, this.capacity);
+			//E[] tempArray = this.list;
+			//this.list = (E[]) new Object[this.capacity * 2];
+			//for(int i = 0; i < tempArray.length; i++) this.list[i] = tempArray[i];
+			
+			//this.size = (this.capacity) / 2;
+			//TODO: Why out of bounds at 10?
+			//IS this 
+		}
 		
 	}
 
 	@Override
 	public E get(int index) {
-		//return list[index];
-		return arrayList.get(index);
+		return list[index];
+		//return arrayList.get(index);
 	}
 
 	@Override
 	public int size() {
-		return arrayList.size();
+		return this.size;
+		//return arrayList.size();
+	}
+	
+	public int capacity(){
+		return this.capacity;
 	}
 
 }
--- PackScheduler/test/edu/ncsu/csc216/pack_scheduler/util/ArrayListTest.java
+++ PackScheduler/test/edu/ncsu/csc216/pack_scheduler/util/ArrayListTest.java
@@ -7,8 +7,52 @@ import org.junit.Test;
 public class ArrayListTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testArrayList() {
+	ArrayList<String> list = new ArrayList<String>();
+	assertEquals(0, list.size());
+	assertNull(list.get(0));
+	assertEquals(10, list.capacity());
+	}
+	
+	@Test
+	public void testAdd(){
+		ArrayList<String> list = new ArrayList<String>();
+		list.add(0, "Bleh");
+		//test that the size increased by 1 
+		assertEquals(1, list.size());
+		//test that the capacity did not change
+		assertEquals(10, list.capacity());
+		//test that the object was added to the correct index
+		assertEquals(list.get(0), "Bleh");
+		
+		//test adding to list less than capacity.
+		list = new ArrayList<String>();
+		list.add(0, "a");
+		list.add(1, "b");
+		list.add(2, "c");
+		list.add(3, "d");
+		list.add(4, "e");
+		list.add(5, "f");
+		list.add(6, "g");
+		list.add(7, "h");
+		list.add(8, "i");
+		list.add(9, "j");
+		//test that the capacity has not changed
+		assertEquals(10, list.size());
+		assertEquals(20, list.capacity());
+		list.add(10, "lastplace");
+		
+		//test adding at zero index
+		list.add(0, "z");
+		//test that the size increased
+		assertEquals(12, list.size());
+		//test that the capacity did not double
+		assertEquals(20, list.capacity());
+		//test that the list correctly placed the values.
+		String[] values = {"z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "lastplace"};
+		for(int i = 0; i < values.length; i++) assertEquals(values[i], list.get(i));
+		
+		
 	}
 
 }
