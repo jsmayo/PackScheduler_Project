37d124afdc585bb807cc51a8ee2cd59645cd9f38
jsmayo <Steven@192.168.0.3>
2017-04-06 11:24:49 -0400

    Implemented FacultyDirectory class

--- PackScheduler/src/edu/ncsu/csc216/pack_scheduler/directory/FacultyDirectory.java
+++ PackScheduler/src/edu/ncsu/csc216/pack_scheduler/directory/FacultyDirectory.java
@@ -1,7 +1,14 @@
 package edu.ncsu.csc216.pack_scheduler.directory;
 
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+import edu.ncsu.csc216.pack_scheduler.io.FacultyRecordIO;
 import edu.ncsu.csc216.pack_scheduler.user.Faculty;
+import edu.ncsu.csc216.pack_scheduler.user.User;
 import edu.ncsu.csc216.pack_scheduler.util.LinkedList;
 
 /**
@@ -20,10 +27,142 @@ public class FacultyDirectory {
 	 * Creates an empty faculty directory.
 	 */
 	public FacultyDirectory() {
+		this.facultyDirectory = new LinkedList<Faculty>();
 	}
+	
+	
 	
 	/**
 	 * Creates an empty faculty directory.  All faculty in the previous
 	 * list are lost unless saved by the user.
 	 */
+	public void newFacultyDirectory() {
+		this.facultyDirectory = new LinkedList<Faculty>();
+	}
+	
+	/**
+	 * Attempts to populate the FacultyDirectory object with values
+	 * parsed from the specified filename.
+	 * @param filename Filename to load into FacultyDirectory.
+	 * @throws IOException Thrown if the specified file cannot be accessed.
+	 */
+	public void loadFacultyFromFile(String filename) {
+		try {
+			facultyDirectory = FacultyRecordIO.readFacultyRecords(filename);
+		} catch (FileNotFoundException e) {
+			throw new IllegalArgumentException("Unable to read file " + filename);
+		}
+	}
+	
+	/**
+	 * Adds a Faculty to the directory.  Returns true if the faculty is added and false if
+	 * the faculty is unable to be added because their id matches another faculty's id.
+	 * 
+	 * This method also hashes the faculty's password for internal storage.
+	 * 
+	 * @param firstName faculty's first name
+	 * @param lastName faculty's last name
+	 * @param id faculty's id
+	 * @param email faculty's email
+	 * @param password faculty's password
+	 * @param repeatPassword faculty's repeated password
+	 * @param maxCredits faculty's max credits.
+	 * @return true if added
+	 */
+	public boolean addFaculty(String firstName, String lastName, String id, String email, String password, String repeatPassword, int maxCourses) {
+		String hashPW = "";
+		String repeatHashPW = "";
+		if (password == null || repeatPassword == null || password.equals("") || repeatPassword.equals("")) {
+			throw new IllegalArgumentException("Invalid password");
+		}
+		try {
+			MessageDigest digest1 = MessageDigest.getInstance(HASH_ALGORITHM);
+			digest1.update(password.getBytes());
+			hashPW = new String(digest1.digest());
+			
+			MessageDigest digest2 = MessageDigest.getInstance(HASH_ALGORITHM);
+			digest2.update(repeatPassword.getBytes());
+			repeatHashPW = new String(digest2.digest());
+		} catch (NoSuchAlgorithmException e) {
+			throw new IllegalArgumentException("Cannot hash password");
+		}
+		
+		if (!hashPW.equals(repeatHashPW)) {
+			throw new IllegalArgumentException("Passwords do not match");
+		}
+		
+		//If an IllegalArgumentException is throw, it's passed up from Faculty
+		//to the GUI
+		Faculty faculty = new Faculty(firstName, lastName, id, email, hashPW, maxCourses);
+		
+		//For loop to see if the faculty ID is already implemented. If so, then false is returned.
+		for (int i = 0; i < facultyDirectory.size(); i++) {
+			User s = facultyDirectory.get(i);
+			if (s.getId().equals(faculty.getId())) {
+				return false;
+			}
+		}
+		return facultyDirectory.add(faculty);
+	}
+	
+	/**
+	 * Removes the faculty with the given id from the list of faculty with the given id.
+	 * Returns true if the faculty is removed and false if the faculty is not in the list.
+	 * @param facultyId faculty's id
+	 * @return true if removed
+	 */
+	public boolean removeFaculty(String facultyId) {
+		for (int i = 0; i < facultyDirectory.size(); i++) {
+			User s = facultyDirectory.get(i);
+			if (s.getId().equals(facultyId)) {
+				facultyDirectory.remove(i);
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	/**
+	 * Returns all faculty in the directory with a column for first name, last name, and id.
+	 * @return String array containing faculty first name, last name, and id.
+	 */
+	public String[][] getFacultyDirectory() {
+		String [][] directory = new String[facultyDirectory.size()][3];
+		for (int i = 0; i < facultyDirectory.size(); i++) {
+			User s = facultyDirectory.get(i);
+			directory[i][0] = s.getFirstName();
+			directory[i][1] = s.getLastName();
+			directory[i][2] = s.getId();
+		}
+		return directory;
+	}
+	
+	/**
+	 * Saves all faculty in the directory to a file.
+	 * @param fileName name of file to save faculty to.
+	 */
+	public void saveFacultyDirectory(String fileName) {
+		try {
+			FacultyRecordIO.writeFacultyRecords(fileName, facultyDirectory);
+		} catch (IOException e) {
+			throw new IllegalArgumentException("Unable to write to file " + fileName);
+		}
+	}
+	
+	/**
+	 * Returns the faculty object associated with the ID string value that's passed
+	 * in as an argument.
+	 * @param id The id of the faculty.
+	 * @return null if the id does not match a faculty within the faculty directory.
+	 * Otherwise, the Faculty object will be returned. 
+	 */
+	public Faculty getFacultyById(String id) {
+		for(int i = 0; i < facultyDirectory.size(); i++){
+			Faculty s = facultyDirectory.get(i);
+			if(s.getId().equals(id)) return s;
+			else continue;
+		}
+		return null;
+	}
+	
 }
